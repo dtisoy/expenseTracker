3f9457d400431a0cc6878dfdaee0b87ccb8424d7
Daniel Tisoy <danieltisoy2001@gmail.com>
2023-10-19 18:20:05 -0500

    fix: add row in transaction list

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
-// get the button
 const btnAddTransaction = getHtmlIds("add-transaction");
 const listTransactions = getHtmlIds("transaction-list");
-// add event listener on click
-btnAddTransaction.addEventListener("click", addTransaction)
-// create a function for the event listener
-// function adds another row with the given data in transaction list
-function addTransaction() {
-    // get the amount input and desc input values
-    const inpAmount = getHtmlIds("amount");
-    const inpDescription = getHtmlIds("description");
-    // update list
-    // get a copy of a list item
-    let itemli = document.querySelector(".list-item").cloneNode(true);
-    let [itemliAmunt, itemliDesc] = itemli.children;
-    [itemliAmunt.textContent, itemliDesc.textContent] = [inpAmount.value, inpDescription.value];
+/* form input */
+const inpAmount = getHtmlIds("amount");
+const inpDescription = getHtmlIds("description");
+
+btnAddTransaction.addEventListener("click", () => addTransactionRow(inpAmount.value, inpDescription.value))
+
+/* functions */
+function addTransactionRow(amount, description) {
+    let itemli = document.createElement("li");
+    itemli.setAttribute("class", "list-item t-normal");
+
+    let itemliAmount = document.createElement("span");
+    let itemliDesc = document.createElement("span");
+
+    [itemliAmount.textContent, itemliDesc.textContent] = [amount, description];
+
+    // add amount and description to row item
+    itemli.appendChild(itemliAmount);
+    itemli.appendChild(itemliDesc);
     // add new row
     listTransactions.appendChild(itemli);
-    // clear input fields
-    [inpAmount.value, inpDescription.value] = ["", ""];
-
-
 
 }
 
