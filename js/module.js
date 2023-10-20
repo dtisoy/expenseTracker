6d6e9a07b7493303856f4176d46840a251716834
Daniel Tisoy <danieltisoy2001@gmail.com>
2023-10-19 19:52:33 -0500

    add: save transaction in localStorage

--- js/module.js
+++ js/module.js
@@ -1,7 +1,15 @@
+/* data*/
+let userExpenseData = getUserData();
 /* balance card */
 let txtBalance = getHtmlIds("balance-total");
 let txtIncome = getHtmlIds("income-total");
 let txtExpense = getHtmlIds("expense-total");
+
+
+function getUserData() {
+    let data = localStorage.getItem("userExpenseData");
+    return JSON.parse(data);
+}
 
 function getHtmlIds(elementId) {
     return document.getElementById(elementId);
@@ -26,4 +34,16 @@ function addTransactionRow(amount, description) {
 
 }
 
-export { txtBalance, txtIncome, txtExpense, getHtmlIds, addTransactionRow }
+function saveTransaction(amount, description) {
+    let { transactions } = userExpenseData;
+    transactions.push({ amount, description });
+    saveSesionData(userExpenseData);
+}
+
+function saveSesionData(data) {
+
+    let jsonStr = JSON.stringify(data);
+    localStorage.setItem("userExpenseData", jsonStr);
+}
+
+export { userExpenseData, txtBalance, txtIncome, txtExpense, getHtmlIds, addTransactionRow, getUserData, saveTransaction, saveSesionData }
