421eb21200ddd2dc3ad7c5850d38eb0edbb3a057
Daniel Tisoy <danieltisoy2001@gmail.com>
2023-10-19 21:08:30 -0500

    update: show data and update user data

--- js/module.js
+++ js/module.js
@@ -37,7 +37,23 @@ function addTransactionRow(amount, description) {
 function saveTransaction(amount, description) {
     let { transactions } = userExpenseData;
     transactions.push({ amount, description });
-    saveSesionData(userExpenseData);
+
+}
+
+function updateExpenseData(userExpense) {
+    let money = []
+    userExpense.transactions.forEach(({ amount }) => {
+        money.push(parseInt(amount));
+    });
+    // get balance
+    // a, c -> accumulator, current value
+    userExpense.balance = money.reduce((a, c) => a + c);
+    // get income
+    userExpense.income = money.filter(n => n > 0).reduce((a, c) => a + c);
+    // get expense 
+    userExpense.expense = money.filter(n => n < 0).reduce((a, c) => a + c);
+
+
 }
 
 function saveSesionData(data) {
@@ -46,4 +62,17 @@ function saveSesionData(data) {
     localStorage.setItem("userExpenseData", jsonStr);
 }
 
-export { userExpenseData, txtBalance, txtIncome, txtExpense, getHtmlIds, addTransactionRow, getUserData, saveTransaction, saveSesionData }
+function showData({ balance, expense, income, transactions }) {
+    txtBalance.textContent = balance;
+    txtExpense.textContent = expense;
+    txtIncome.textContent = income;
+    showTransactionList(transactions)
+
+}
+
+function showTransactionList(list) {
+    list.forEach(({ amount, description }) => {
+        addTransactionRow(amount, description)
+    });
+}
+export { userExpenseData, getHtmlIds, getUserData, saveTransaction, saveSesionData, updateExpenseData, showData }
